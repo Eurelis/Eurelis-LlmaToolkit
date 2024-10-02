b71934dc2c35e6303c748ceee79ee877879cc3d9
Vincent Lambert <v.lambert@eurelis.com>
2024-10-02 17:17:05 +0200

    Code review

--- src/eurelis_llmatoolkit/llamaindex/readers/simple_webpage_reader_adapter.py
+++ src/eurelis_llmatoolkit/llamaindex/readers/simple_webpage_reader_adapter.py
@@ -1,6 +1,7 @@
 from llama_index.readers.web import SimpleWebPageReader
 
 
+# TODO: Pour plus de clareté créer une classe abstraite ReaderAdapter
 class SimpleWebPageReaderAdapter:
     def __init__(self, config):
         self.reader = SimpleWebPageReader(
@@ -10,6 +11,7 @@ class SimpleWebPageReaderAdapter:
     def load_data(self, urls):
         return self.reader.load_data(urls=urls)
 
+    # TODO: Généraliser la méthode pour tous les readers au niveau de la classe abstraite
     @staticmethod
     def get_load_data_params(dataset_config):
         return {
--- src/eurelis_llmatoolkit/llamaindex/readers/sitemap_reader_adapter.py
+++ src/eurelis_llmatoolkit/llamaindex/readers/sitemap_reader_adapter.py
@@ -1,6 +1,7 @@
 from llama_index.readers.web import SitemapReader
 
 
+# TODO: Pour plus de clareté créer une classe abstraite ReaderAdapter
 class SitemapReaderAdapter:
     def __init__(self, config):
         self.reader = SitemapReader(
@@ -9,8 +10,10 @@ class SitemapReaderAdapter:
         )
 
     def load_data(self, sitemap_url):
+        # TODO: Décompacter les paramètres pour factoriser la méthode au niveau de la classe abstraite
         return self.reader.load_data(sitemap_url=sitemap_url)
 
+    # TODO: Généraliser la méthode pour tous les readers au niveau de la classe abstraite
     @staticmethod
     def get_load_data_params(dataset_config):
         return {
