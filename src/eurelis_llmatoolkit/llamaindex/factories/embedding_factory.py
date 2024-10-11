5541ae13bfc1e5ab229b53f0ad654f1f19e2ce0b
Mickaël Morel <m.morel@eurelis.com>
2024-10-11 18:12:03 +0200

    EURLLMATK-27 LlamaIndex : embeddings Hugging Face

--- src/eurelis_llmatoolkit/llamaindex/factories/embedding_factory.py
+++ src/eurelis_llmatoolkit/llamaindex/factories/embedding_factory.py
@@ -1,4 +1,5 @@
 from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
 class EmbeddingFactory:
@@ -10,6 +11,13 @@ class EmbeddingFactory:
             return OpenAIEmbedding(
                 model=config.get("model", "text-embedding-3-small"),
                 api_key=config["openai_api_key"],
+            )
+        if provider == "HuggingFace":
+            return HuggingFaceEmbedding(
+                model_name=config.get(
+                    "model", "antoinelouis/biencoder-electra-base-french-mmarcoFR"
+                ),
+                cache_folder=config["cache_folder"],
             )
 
         raise ValueError(f"Embedding provider {provider} non supporté.")
