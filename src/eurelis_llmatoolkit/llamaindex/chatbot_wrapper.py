eaec844e1ed2ea0939dbe2b4c20451ff8e747ce1
NicolasLoisyEurelis <n.loisy@eurelis.com>
2024-10-14 17:18:17 +0200

    LlamaIndex : ChatbotWrapper (Memory and ChatEngine)

--- src/eurelis_llmatoolkit/llamaindex/chatbot_wrapper.py
+++ src/eurelis_llmatoolkit/llamaindex/chatbot_wrapper.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING, Optional
 from llama_index.core import VectorStoreIndex
+from llama_index.core.chat_engine.types import ChatMode
+from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.storage import StorageContext
 from llama_index.core.query_engine import RetrieverQueryEngine
@@ -32,25 +34,56 @@ class ChatbotWrapper:
         self._embedding_model: "BaseEmbedding" = None
         self._llm: "BaseLLM" = None
         self._query_engine: Optional[RetrieverQueryEngine] = None
+        self._memory: Optional[ChatMemoryBuffer] = None
+        self._chat_engine = None
 
     def run(self):
         """
         Runs the chatbot by initializing the vector store, storage context,
-        index, retriever, and query engine.
+        index, retriever, query engine, and memory.
         """
         vector_store = self._get_vector_store()
         storage_context = self._get_storage_context()
         index = self._get_index()
         retriever = self._get_retriever()
         llm = self._get_llm()
-        query_engine = self._create_query_engine(retriever, llm)
+        memory = self._get_memory()
+        query_engine = self._get_query_engine(retriever, llm)
+        chat_engine = self._get_chat_engine()
 
         print(f"VectorStore initialized: {vector_store}")
         print(f"StorageContext initialized: {storage_context}")
         print(f"Index initialized: {index}")
         print(f"Retriever initialized: {retriever}")
         print(f"LLM initialized: {llm}")
+        print(f"Memory initialized: {memory}")
         print(f"QueryEngine initialized: {query_engine}")
+        print(f"ChatEngine initialized: {chat_engine}")
+
+        # FIXME : Empty Response
+        response = chat_engine.chat("Hello!")
+        print(response)
+
+        response = chat_engine.chat("What is Drupal about?")
+        print(response)
+
+        response = chat_engine.chat("Can you tell me more?")
+        print(response)
+
+    def _get_memory(self):
+        """
+        Creates a ChatMemoryBuffer with a token limit.
+
+        Returns:
+            ChatMemoryBuffer: The configured memory buffer.
+        """
+        if self._memory is not None:
+            return self._memory
+
+        # Initialize ChatMemoryBuffer with a token limit of 1500
+        self._memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
+
+        return self._memory
 
     def _get_vector_store(self):
         """
@@ -89,7 +122,7 @@ class ChatbotWrapper:
 
     def _get_storage_context(self):
         """
-        Creates a StorageContext using the vector store and the document store.
+        Creates a StorageContext using the vector store and document store.
 
         Returns:
             StorageContext: The configured storage context.
@@ -97,7 +130,7 @@ class ChatbotWrapper:
         if self._storage_context is not None:
             return self._storage_context
 
-        # Get the vector store and the document store
+        # Get the vector store and document store
         vector_store = self._get_vector_store()
         document_store = self._get_document_store()
 
@@ -110,7 +143,7 @@ class ChatbotWrapper:
 
     def _get_index(self):
         """
-        Creates a VectorStoreIndex using the vector store and the storage context.
+        Creates a VectorStoreIndex using the vector store and storage context.
 
         Returns:
             VectorStoreIndex: The configured index.
@@ -121,7 +154,7 @@ class ChatbotWrapper:
         storage_context = self._get_storage_context()
         vector_store = self._get_vector_store()
 
-        # Create the index using the vector store and the storage context
+        # Create the index using the vector store and storage context
         self._index = VectorStoreIndex.from_vector_store(
             vector_store, storage_context=storage_context
         )
@@ -146,7 +179,7 @@ class ChatbotWrapper:
 
     def _get_retriever(self):
         """
-        Creates a VectorIndexRetriever using the index and the embedding model.
+        Creates a VectorIndexRetriever using the index and embedding model.
 
         Returns:
             VectorIndexRetriever: The configured retriever.
@@ -183,9 +216,9 @@ class ChatbotWrapper:
 
         return self._llm
 
-    def _create_query_engine(self, retriever, llm):
+    def _get_query_engine(self, retriever, llm):
         """
-        Creates a query engine using the retriever and the language model.
+        Creates a query engine using the retriever and language model.
 
         Args:
             retriever: The retriever to be used for querying.
@@ -202,3 +235,28 @@ class ChatbotWrapper:
         )
 
         return self._query_engine
+
+    def _get_chat_engine(self):
+        """
+        Creates a chat engine using the index, chat mode, memory, and system prompt.
+
+        Returns:
+            ChatEngine: The configured chat engine.
+        """
+        if self._chat_engine is not None:
+            return self._chat_engine
+
+        index = self._get_index()
+        memory = self._get_memory()
+
+        # Create the chat engine with the specified configuration
+        self._chat_engine = index.as_chat_engine(
+            chat_mode=ChatMode.CONTEXT,
+            memory=memory,
+            system_prompt=(
+                "You are a chatbot, able to have normal interactions, as well as talk "
+                "about an essay discussing Paul Graham's life."
+            ),
+        )
+
+        return self._chat_engine
