c7ac7017cec6d8907354b1b18b3a77a53036097c
Mickaël Morel <m.morel@eurelis.com>
2024-10-15 16:44:16 +0200

    EURLLMATK-45 LlamaIndex : factory réflexive pour les readers

---
+++ src/eurelis_llmatoolkit/llamaindex/factories/poc_reader_factory.py
@@ -0,0 +1,23 @@
+import importlib
+import inspect
+
+
+class PocReaderFactory:
+    @staticmethod
+    def create_reader(config: dict, namespace: str):
+        provider_path = config["provider"]
+
+        if provider_path.count(".") == 0:
+            raise ValueError("Reader provider path must contain one dot")
+
+        module_name, class_name = provider_path.rsplit(".", 1)
+
+        module = importlib.import_module(module_name)
+
+        reader_class = getattr(module, class_name)
+
+        init_params = inspect.signature(reader_class.__init__)
+
+        if "namespace" in init_params.parameters:
+            return reader_class(config, namespace)
+        return reader_class(config)
--- src/eurelis_llmatoolkit/llamaindex/readers/__init__.py
+++ src/eurelis_llmatoolkit/llamaindex/readers/__init__.py
@@ -8,10 +8,14 @@ from eurelis_llmatoolkit.llamaindex.readers.simple_webpage_reader_adapter import
 from eurelis_llmatoolkit.llamaindex.readers.sitemap_reader_adapter import (
     SitemapReaderAdapter,
 )
+from eurelis_llmatoolkit.llamaindex.readers.txt_file_reader import TXTFileReader
+from eurelis_llmatoolkit.llamaindex.readers.pdf_file_reader import PDFFileReader
 
 __all__ = [
     "AdvancedSitemapReader",
     "ReaderAdapter",
     "SimpleWebPageReaderAdapter",
     "SitemapReaderAdapter",
+    "TXTFileReader",
+    "PDFFileReader",
 ]
