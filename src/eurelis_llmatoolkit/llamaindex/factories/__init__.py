198c1abb08c2a422a9ed8c9c69cda3657e489873
NicolasLoisyEurelis <n.loisy@eurelis.com>
2024-10-21 13:59:05 +0200

    LlamaIndex : memory and retriever in init factories

--- src/eurelis_llmatoolkit/llamaindex/factories/__init__.py
+++ src/eurelis_llmatoolkit/llamaindex/factories/__init__.py
@@ -1,7 +1,9 @@
 from eurelis_llmatoolkit.llamaindex.factories.cache_factory import CacheFactory
 from eurelis_llmatoolkit.llamaindex.factories.embedding_factory import EmbeddingFactory
 from eurelis_llmatoolkit.llamaindex.factories.llm_factory import LLMFactory
+from eurelis_llmatoolkit.llamaindex.factories.memory_factory import MemoryFactory
 from eurelis_llmatoolkit.llamaindex.factories.reader_factory import ReaderFactory
+from eurelis_llmatoolkit.llamaindex.factories.retriever_factory import RetrieverFactory
 from eurelis_llmatoolkit.llamaindex.factories.transformation_factory import (
     TransformationFactory,
 )
@@ -13,7 +15,9 @@ __all__ = [
     "CacheFactory",
     "EmbeddingFactory",
     "LLMFactory",
+    "MemoryFactory",
     "ReaderFactory",
+    "RetrieverFactory",
     "TransformationFactory",
     "VectorStoreFactory",
 ]
