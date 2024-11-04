ae5e6e508f6db9b39373950bfe764ff2058b3bbd
Mickaël Morel <m.morel@eurelis.com>
2024-11-04 16:34:53 +0100

    Lazy imports

--- src/eurelis_llmatoolkit/llamaindex/factories/documentstore_factory.py
+++ src/eurelis_llmatoolkit/llamaindex/factories/documentstore_factory.py
@@ -1,12 +1,11 @@
-from llama_index.storage.docstore.mongodb import MongoDocumentStore
-
-
 class DocumentStoreFactory:
     @staticmethod
     def create_document_store(config: dict):
         provider = config["provider"]
 
         if provider == "MongoDB":
+            from llama_index.storage.docstore.mongodb import MongoDocumentStore
+
             return MongoDocumentStore.from_uri(
                 uri=config["url"],
                 db_name=config["db_name"],
--- src/eurelis_llmatoolkit/llamaindex/factories/embedding_factory.py
+++ src/eurelis_llmatoolkit/llamaindex/factories/embedding_factory.py
@@ -1,18 +1,18 @@
-from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-
-
 class EmbeddingFactory:
     @staticmethod
     def create_embedding(config: dict):
         provider = config["provider"]
 
         if provider == "OpenAI":
+            from llama_index.embeddings.openai import OpenAIEmbedding
+
             return OpenAIEmbedding(
                 model=config.get("model", "text-embedding-3-small"),
                 api_key=config["openai_api_key"],
             )
         if provider == "HuggingFace":
+            from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
             return HuggingFaceEmbedding(
                 model_name=config.get(
                     "model", "antoinelouis/biencoder-electra-base-french-mmarcoFR"
