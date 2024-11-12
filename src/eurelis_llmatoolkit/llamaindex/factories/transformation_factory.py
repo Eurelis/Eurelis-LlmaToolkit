3bf020a9e73588c8370d35d14e31b18c731293cc
NicolasLoisyEurelis <n.loisy@eurelis.com>
2024-11-12 14:08:37 +0100

    LlamaIndex [105] : All LLM with structured response

--- src/eurelis_llmatoolkit/llamaindex/factories/transformation_factory.py
+++ src/eurelis_llmatoolkit/llamaindex/factories/transformation_factory.py
@@ -1,30 +1,31 @@
-from llama_index.core.node_parser import SentenceSplitter
-
-from eurelis_llmatoolkit.llamaindex.transformers.json_file_acronym_transformer import (
-    JSONFileAcronymTransformer,
-)
-from eurelis_llmatoolkit.llamaindex.transformers.metadata_transformer import (
-    MetadataTransformer,
-)
-from eurelis_llmatoolkit.llamaindex.transformers.node_llm_transformer import (
-    NodeLLMTransformer,
-)
-
-
 class TransformationFactory:
     @staticmethod
     def create_transformation(config: dict):
         provider = config["provider"]
 
         if provider == "SentenceSplitter":
+            from llama_index.core.node_parser import SentenceSplitter
+
             return SentenceSplitter(
                 chunk_size=config.get("chunk_size", 768),
                 chunk_overlap=config.get("chunk_overlap", 56),
             )
         if provider == "JSONFileAcronymTransformer":
+            from eurelis_llmatoolkit.llamaindex.transformers.json_file_acronym_transformer import (
+                JSONFileAcronymTransformer,
+            )
+
             return JSONFileAcronymTransformer(config)
         if provider == "MetadataTransformer":
+            from eurelis_llmatoolkit.llamaindex.transformers.metadata_transformer import (
+                MetadataTransformer,
+            )
+
             return MetadataTransformer(config)
-        if provider == "NodeLLMTransformer":
-            return NodeLLMTransformer(config)
+        if provider == "LLMNodeTransformer":
+            from eurelis_llmatoolkit.llamaindex.transformers.llm_node_transformer import (
+                LLMNodeTransformer,
+            )
+
+            return LLMNodeTransformer(config)
         raise ValueError(f"Transformation provider {provider} is not supported.")
